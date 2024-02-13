29d247d830188ea0aa6b34b5ccd7fa92bb4329d9
Nicolle Policiano <nicolle.fabri@gmail.com>
2024-02-13 11:20:46 -0600

    IOS-3: Create notRated state for the score view

--- TMDB/ScoreView.swift
+++ TMDB/ScoreView.swift
@@ -8,35 +8,45 @@
 import SwiftUI
 
 struct ScoreView: View {
-    let progress: Double
-    let ringColor = Color(red: 99/255, green: 205/255, blue: 130/255)
-    let backgroundColor = Color(red: 13/255, green: 28/255, blue: 33/255)
+    let progress: Double?
+    private let ringColor = Color(red: 99/255, green: 205/255, blue: 130/255)
+    private let backgroundColor = Color(red: 13/255, green: 28/255, blue: 33/255)
+    private let notRatedRingColor = Color(red: 102/255, green: 102/255, blue: 102/255)
 
     var body: some View {
         ZStack {
             Circle()
                 .stroke(
-                    ringColor.opacity(0.5),
+                    progress != nil 
+                        ? ringColor.opacity(0.5)
+                        : notRatedRingColor,
                     lineWidth: 2
                 )
-            Circle()
-                .trim(from: 0, to: progress)
-                .stroke(
-                    ringColor,
-                    style: StrokeStyle(
-                        lineWidth: 2,
-                        lineCap: .round
+            if let progress {
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(
+                        ringColor,
+                        style: StrokeStyle(
+                            lineWidth: 2,
+                            lineCap: .round
+                        )
                     )
-                )
-                .rotationEffect(.degrees(-90))
-            Text(String(Int(progress*100)))
-                .font(.caption)
-                .fontWeight(.bold)
-                .foregroundStyle(.white)
-            + Text("%")
-                .font(.system(size: 5))
-                .foregroundStyle(.white)
-                .baselineOffset(6.0)
+                    .rotationEffect(.degrees(-90))
+                Text(String(Int(progress*100)))
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                + Text("%")
+                    .font(.system(size: 5))
+                    .foregroundStyle(.white)
+                    .baselineOffset(6.0)
+            } else {
+                Text("NR")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+            }
         }
         .frame(width: 30, height: 30)
         .background(
@@ -55,6 +65,6 @@ struct ScoreView: View {
     traits: .fixedLayout(width: 100, height: 100)
 )
 {
-    ScoreView(progress: 1)
+    ScoreView(progress: nil)
         .padding()
 }
